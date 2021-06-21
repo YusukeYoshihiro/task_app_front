c00c0c8940556f345f976f2f08b64ce3952abed2
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2021-06-20 22:58:11 -0700

    Changes to be committed: 	modified:   src/features/task/taskSlice.ts

--- src/features/task/taskSlice.ts
+++ src/features/task/taskSlice.ts
@@ -201,7 +201,7 @@ export const taskSlice = createSlice({
        }
       }
     );
-    // JWTのトークン認証のアクセスタイム（３０分）が経過した場合、再度ログインを促す。
+    // JWTのトークン認証のアクセスタイム（３０min）が経過した場合、再度ログインを促す。
     builder.addCase(fetchAsyncGetTasks.rejected, ()=> {
       window.location.href = '/';
     });
